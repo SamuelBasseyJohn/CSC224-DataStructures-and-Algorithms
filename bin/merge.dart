8d3c2796251946b85dbb6676fb69d32bdadd87e1
SamuelBasseyJohn <88030818+SamuelBasseyJohn@users.noreply.github.com>
2023-05-21 17:33:40 +0100

    Stack Data Structure

--- bin/merge.dart
+++ bin/merge.dart
@@ -1,5 +1,22 @@
+import 'package:dsa_implementations/stack.dart';
+
 void main(List<String> args) {
-  print('we good');
-  print("Hello World!");
-  print("Hopefully this works well");
+  var pages = Stack<String>(5);
+  print(pages);
+  print(pages.length());
+  pages.push("Page 1");
+  pages.push("Page 2");
+  print(pages);
+  print(pages.length());
+  pages.pop();
+  print(pages);
+  print(pages.length());
+  pages.push("Page 3");
+  pages.push("Page 4");
+  pages.push("Page 5");
+  pages.push("Page 6");
+  print(pages);
+  print(pages.length());
+  pages.push("Page 7");
+  pages.push("Page 8");
 }
---
+++ lib/stack.dart
@@ -0,0 +1,29 @@
+class Stack<T> {
+  final List<T?> _container;
+  int _top = -1;
+
+  Stack(int size) : _container = List.filled(size, null);
+
+  void push(T item) {
+    if (_top < _container.length - 1) {
+      _top++;
+      _container[_top] = item;
+    } else {
+      throw StackOverflowError();
+    }
+  }
+
+  void pop() {
+    if (_top > -1) {
+      _container[_top] = null;
+      _top--;
+    } else {
+      return;
+    }
+  }
+
+  String length() => "${_top + 1}";
+
+  @override
+  String toString() => "$_container";
+}
